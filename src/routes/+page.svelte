c6e37009bee1adcec004e8786f938306a447c668
jaqb8 <qba.anisz@gmail.com>
2023-08-31 13:54:47 +0200

    change theme & landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,22 @@
   import { AuthCheck } from "$lib/components";
 </script>
 
-<h1 class="text-green-400">Welcome to SvelteKit</h1>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <div class="flex justify-center items-center gap-3">
+        <img src="/logo.svg" alt="LinkVerse logo" class="w-10" />
+        <h1 class="text-5xl font-bold">LinkVerse</h1>
+      </div>
+      <h3 class="mt-2 font-bold text-secondary">
+        One Link, Infinite Possibilities
+      </h3>
+      <p class="py-6">
+        Gone are the days of juggling numerous links. Bring them under one roof
+        and share your entire online world with just one URL.
+      </p>
 
-<AuthCheck>yoyoyo</AuthCheck>
+      <a href="/login" class="btn btn-primary">Get Started</a>
+    </div>
+  </div>
+</div>
